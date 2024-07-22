f1208ac3069125d3c3fb5d1fccf87a2066cbbf1f
Navid <navidnaseri007@gmail.com>
2024-07-22 05:08:15 -0400

    final fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './index.css';
 import * as jsonpatch from 'fast-json-patch';
 import TestJson from "./TestJson";
@@ -8,15 +8,19 @@ function App() {
 
     const [patch, setPatch] = useState({})
     const [duration, setDuration] = useState(0)
-    
+
+
+    useEffect(() => {
+        document.title = "Json BenchMark";
+    }, []);
     
     var json = TestJson();
     
-    const ChangeClick = (e) => {
+    const ChangeClick = (e,runCount) => {
         
         const startTime = Date.now();
 
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < runCount; i++) {
             var changesJson = randomChangeJson(json);
             var patch = generatePatch(json,changesJson);
             setPatch(patch);
@@ -49,36 +53,51 @@ function App() {
             }
         };
 
+        const getRandomKey = () => {
+            return Math.random().toString(36).substring(2, 7);
+        };
+
         const traverseAndChange = (obj) => {
-            for (let key in obj) {
-                if (obj.hasOwnProperty(key)) {
-                    if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
-                        traverseAndChange(obj[key]);
-                    } else {
-                        obj[key] = getRandomValue(obj[key]);
-                    }
+            const keys = Object.keys(obj);
+            keys.forEach(key => {
+                if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+                    traverseAndChange(obj[key]);
+                } else {
+                    obj[key] = getRandomValue(obj[key]);
                 }
+
+                // Randomly decide to remove a key
+                if (Math.random() > 0.8) {
+                    delete obj[key];
+                }
+            });
+
+            // Randomly decide to add a new key-value pair
+            if (Math.random() > 0.8) {
+                const newKey = getRandomKey();
+                obj[newKey] = getRandomValue(null);
             }
         };
 
-        const newJson = JSON.parse(JSON.stringify(json)); // Deep copy of the original JSON
+        const newJson = JSON.parse(JSON.stringify(json)); 
         traverseAndChange(newJson);
         return newJson;
     };
     
     return (
-        <div>
-            <button onClick={event => ChangeClick(event)}>change</button>
+        <div className="container">
+            <button className="button" onClick={event => ChangeClick(event,1)}>Change Once</button>
+            <button className="button" onClick={event => ChangeClick(event,5)}>Change 5 Times</button>
             <br/>
             <br/>
-            <label>Change Patch:</label>
+            <label className="label">Time:</label>
             <br/>
-            {patch && <span>{JSON.stringify(patch)}</span>}
+            <span className="text">{duration} milliseconds</span>
             <br/>
             <br/>
-            <label>time:</label>
+            <label className="label">Created Json Patch:</label>
             <br/>
-            <span>{duration} milliseconds</span>
+            {patch && <span className="text">{JSON.stringify(patch)}</span>}
         </div>
 
     )
